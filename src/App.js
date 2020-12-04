a8ecd72c2df764031f2df50027dfaa6ac3aa68b1
Steven Rodriguez <srrodrig.rodriguez@gmail.com>
2020-12-04 17:27:27 -0600

    Created index and app js files

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { func } from 'prop-types';
+import React from 'react';
+import Directory from './components/Directory';
+
+function App() {
+    return <Directory />;
+}
+
+export default App;
